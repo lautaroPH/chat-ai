619354257baa4616368937140b4ab43008ac4e63
Lautaro Perez Herrera <lautaroph@gmail.com>
2023-10-27 13:19:25 -0300

    a

--- config.js
+++ config.js
@@ -2,5 +2,4 @@ export const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL;
 export const SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 export const GOOGLE_MAPS_API_KEY = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY;
 export const OPENAI_API_KEY = process.env.NEXT_PUBLIC_OPENAI_API_KEY;
-export const GOOGLE_ANALYTICS = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS;
 export const SERVICE_ROLE = process.env.SUPABASE_SERVICE_ROLE;
